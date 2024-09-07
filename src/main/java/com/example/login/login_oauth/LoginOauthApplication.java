e1467851e0e2894ff2790bc377b9db6961a3f27e
Shuaib <shuaib6950@gmail.com>
2024-09-07 20:27:02 +0600

    Initial

---
+++ src/main/java/com/example/login/login_oauth/LoginOauthApplication.java
@@ -0,0 +1,14 @@
+package com.example.login.login_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginOauthApplication.class, args);
+		System.out.println("Application has been started-----");
+	}
+
+}
